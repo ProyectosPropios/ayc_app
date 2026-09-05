9880731ac8e9a2061b965ce82939cba3e578d081
jonier255 <jonier145@gmail.com>
2026-09-05 15:33:35 -0500

    Forzar configuración correcta de Django en producción

--- ayc_api/celery.py
+++ ayc_api/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ayc_api.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "ayc_api.settings"
 
 app = Celery("ayc_api")
 app.config_from_object("django.conf:settings", namespace="CELERY")
