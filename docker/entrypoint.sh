fdfa7e3e670c7edde54fb4469b9faa68b05844ca
jonier255 <jonier145@gmail.com>
2026-09-05 15:26:44 -0500

    Forzar configuración de producción en Docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eu
 
+# El contenedor de despliegue siempre debe usar la configuracion de
+# produccion. Esto evita que una variable DJANGO_SETTINGS_MODULE antigua de
+# Render seleccione accidentalmente settingsDev.py.
+export DJANGO_SETTINGS_MODULE=ayc_api.settings
+
 if [ "${RUN_MIGRATIONS:-0}" = "1" ]; then
     python manage.py migrate --noinput
     python manage.py collectstatic --noinput
